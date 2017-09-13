9d391e2fb6cf3accdda8cde51b8629ed7b82d529
Valerii Atamanchuk <frolapps@gmail.com>
2017-09-13 16:04:13 +0800

    Initial Commit

---
+++ MoviesTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoviesTest
+//
+//  Created by Val on 9/13/17.
+//  Copyright © 2017 frolapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
